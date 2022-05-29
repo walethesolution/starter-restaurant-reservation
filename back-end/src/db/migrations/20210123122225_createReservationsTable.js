86cdf1d0f8a3eb15e0184780e2644a4b30286526
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-05-29 14:21:45 -0400

    Complete migrations of tables

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("first_name");
+    table.string("last_name", null).notNullable();
+    table.string("mobile_number");
+    table.string("reservation_date");
+    table.string("reservation_time");
+    table.integer("people");
+    table.string("status").notNullable().defaultTo("booked");
     table.timestamps(true, true);
   });
 };
